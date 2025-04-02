35cd2aa4996712cdcc15c4a4b1238725f153a551
najminurin <abdullahnajmi12@gmail.com>
2025-04-02 21:10:52 +0800

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,61 @@
+import java.util.LocalDate;
+public class Task{
+    private String taskName ;
+    private String category;
+    private LocalDate dueDate ;
+    private String priority;
+
+    public Task(){
+       taskName = ' ';
+       category = ' ';
+       dueDate = 0 ;
+       priority = ' ';
+    }
+
+    public void Task(String taskName , String category , LocalDate dueDate , String priority){
+         this.taskName = taskName;
+         this.category = category;
+         this.dueDate = dueDate;
+         this.priority = priority;
+    }
+
+    //getter
+
+    public String getTaskName(){
+        return taskName;
+    }
+    public String getCategory(){
+        return category;
+    }
+    public LocalDate getDueDate(){
+        return dueDate;
+    }
+    public String getPriority(){
+        return priority;
+    }
+
+    // setter
+
+    public void setTaskName(String taskName){
+        this.taskName = taskName;
+    }
+
+    public void setCategory(String category){
+        this.category = category;
+    }
+
+    public void setDueDate(Date dueDate){
+        this.dueDate = dueDate;
+    }
+
+    public void setPriority(String priority){
+        this.priority = priority;
+    }
+
+    public String toString(){
+      return "Task Name: " + taskName + "\n" +
+               "Category: " + category + "\n" +
+               "Due Date: " + dueDate + "\n" +
+               "Priority: " + priority + "\n";
+    }
+}
