495d4fae2db29ef4df7e674e3bbea203890fc488
najminurin <abdullahnajmi12@gmail.com>
2025-04-10 08:43:19 +0800

    update

---
+++ TaskValidator.java
@@ -0,0 +1,8 @@
+public class TaskValidator {
+    public static String validateTask(Task task) {
+        if (task.getTaskName() == null || task.getTaskName().trim().isEmpty()) {
+            return "Task cannot be empty!!!";
+        }
+        if (category.getCategory())
+    }
+}
